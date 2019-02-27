54dd1c2195664ba2606fbe416f5a8fe7f85b62d1
JohnEmad <jogo2018@gmail.com>
2019-02-27 16:44:08 +0200

    Adding CommentsController

---
+++ FaceBookSol/FaceBook-InitialVersion/Controllers/CommentsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FaceBook_InitialVersion.Data;
+using FaceBook_InitialVersion.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using static FaceBook_InitialVersion.Models.Enums;
+
+namespace FaceBook_InitialVersion.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Person> _userManager;
+        public CommentsController(ApplicationDbContext context, UserManager<Person> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> AddComment(String commentBody, int postId)
+        {
+            var comment = new Comment()
+            {
+
+                Body = commentBody,
+                CreationDate = DateTime.Now,
+                State = CommentStatus.Active
+
+            };
+            _context.Comments.Add(comment);
+            _context.SaveChanges();
+            var x = comment.ID;
+
+            var userPostComment = new UserPostComment()
+            {
+                CommentID = comment.ID,
+                PostID = postId,
+                userID = _userManager.GetUserId(User)
+            };
+            _context.UserPostComments.Add(userPostComment);
+            _context.SaveChanges();
+            //var commentId = _context.Comments.Where(c => c.CreationDate == comment.CreationDate).Select(c => c.ID).FirstOrDefault();
+            return PartialView("~/Views/Posts/GetAll", await _context.Posts.Include(p => p.User).Include(u => u.UserPostLikes).Include(u => u.UserPostComments).ThenInclude(c => c.Comment).OrderByDescending(p => p.CreationDate).ToListAsync());
+
+        }
+    }
+}
--- FaceBookSol/FaceBook-InitialVersion/Controllers/PostsController.cs
+++ FaceBookSol/FaceBook-InitialVersion/Controllers/PostsController.cs
@@ -170,35 +170,12 @@ namespace FaceBook_InitialVersion.Controllers
             return PartialView("GetAll", await _context.Posts.Include(p => p.User).Include(u => u.UserPostLikes).Include(u => u.UserPostComments).ThenInclude(c => c.Comment).OrderByDescending(p => p.CreationDate).ToListAsync());
 
         }
-        public async Task<IActionResult> AddComment(String commentBody, int postId)
-        {
-            var comment = new Comment()
-            {
-
-                Body = commentBody,
-                CreationDate = DateTime.Now,
-                State = CommentStatus.Active
-
-            };
-            _context.Comments.Add(comment);
-            _context.SaveChanges();
-            var x = comment.ID;
-
-            var userPostComment = new UserPostComment()
-            {
-                CommentID = comment.ID,
-                PostID = postId,
-                userID = _userManager.GetUserId(User)
-            };
-            _context.UserPostComments.Add(userPostComment);
-            _context.SaveChanges();
-            //var commentId = _context.Comments.Where(c => c.CreationDate == comment.CreationDate).Select(c => c.ID).FirstOrDefault();
-            return PartialView("GetAll", await _context.Posts.Include(p => p.User).Include(u => u.UserPostLikes).Include(u => u.UserPostComments).ThenInclude(c => c.Comment).OrderByDescending(p => p.CreationDate).ToListAsync());
-
-        }
+        
         private bool PostExists(int id)
         {
             return _context.Posts.Any(e => e.ID == id);
         }
+
+
     }
 }
